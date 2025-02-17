84e520c419f812bbffb60eb1780663b90d3e5ace
Mathieu Schertenleib <schertenleib.mathieu@gmail.com>
2025-02-17 13:02:54 +0100

    Try computing flow on edges

--- src/optical_flow.py
+++ src/optical_flow.py
@@ -5,6 +5,18 @@ import cv2
 import numpy as np
 
 
+def edge_filter(gray: np.ndarray) -> np.ndarray:
+    grad_x = cv2.Sobel(
+        gray, cv2.CV_32F, 1, 0, ksize=3, scale=1.0, delta=0.0, borderType=cv2.BORDER_DEFAULT
+    )
+    grad_y = cv2.Sobel(
+        gray, cv2.CV_32F, 0, 1, ksize=3, scale=1.0, delta=0.0, borderType=cv2.BORDER_DEFAULT
+    )
+    grad_norm = np.sqrt(np.square(grad_x) + np.square(grad_y))
+    # return grad_norm
+    return (grad_norm > 100.0).astype(np.uint8) * 255
+
+
 def draw_flow_arrows(
     img: np.ndarray, flow: np.ndarray, spacing: int, scale: float, color: Sequence[float]
 ) -> None:
@@ -41,9 +53,14 @@ def main(file_path: str) -> None:
     if not cap.isOpened():
         print(f"Failed to open video '{file_path}'")
         exit()
+
     framerate = cap.get(cv2.CAP_PROP_FPS)
     if framerate == 0.0:
+        print(f"Using video framerate of {framerate} fps")
+    else:
         framerate = 30.0
+        print(f"Unable to determine video framerate, using {framerate} fps")
+
     imgs = []
     while cap.isOpened():
         ret, frame = cap.read()
@@ -52,15 +69,19 @@ def main(file_path: str) -> None:
         imgs.append(frame)
     cap.release()
 
-    img = np.zeros(shape=(imgs[0].shape[0] * 2, imgs[0].shape[1], 3), dtype=np.uint8)
+    img = np.zeros(shape=(imgs[0].shape[0] * 2, imgs[0].shape[1] * 2, 3), dtype=np.uint8)
 
     prev = cv2.cvtColor(imgs[0], cv2.COLOR_RGB2GRAY)
+    edges_prev = edge_filter(prev)
+
     hsv = np.zeros_like(imgs[0])
     hsv[..., 1] = 255
 
     i = 1
     while True:
         next = cv2.cvtColor(imgs[i], cv2.COLOR_RGB2GRAY)
+        edges_next = edge_filter(next)
+
         flow = cv2.calcOpticalFlowFarneback(
             prev=prev,
             next=next,
@@ -78,17 +99,20 @@ def main(file_path: str) -> None:
         hsv[..., 0] = np.rad2deg(ang) / 2
         hsv[..., 2] = cv2.normalize(src=mag, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
 
-        img_up = img[: img.shape[0] // 2, :, :]
-        img_down = img[img.shape[0] // 2 :, :, :]
-        img_up[:] = cv2.cvtColor(imgs[i], cv2.COLOR_RGB2BGR)
+        img_top_left = img[: img.shape[0] // 2, : img.shape[1] // 2, :]
+        img_bottom_left = img[img.shape[0] // 2 :, : img.shape[1] // 2, :]
+        img_top_right = img[: img.shape[0] // 2, img.shape[1] // 2 :, :]
+
+        img_top_left[:] = cv2.cvtColor(imgs[i], cv2.COLOR_RGB2BGR)
         draw_flow_arrows(
-            img=img_up,
+            img=img_top_left,
             flow=flow,
             spacing=10,
             scale=5,
             color=(255, 255, 255),
         )
-        img_down[:] = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        img_bottom_left[:] = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        img_top_right[:] = cv2.cvtColor(edges_next, cv2.COLOR_GRAY2BGR)
 
         cv2.imshow("img", img)
 
